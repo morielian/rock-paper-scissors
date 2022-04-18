b5d252a36205a0280a501c42402016155226bd17
Douglas Purdy <morielian@gmail.com>
2022-04-17 23:57:10 -0400

    added round function and started conditional

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+function playRound() {
+  let choice = computerPlay();
+  const playerChoice =  prompt('What is your choice? Rock, Paper, or Scissors')
+  if (choice == playerChoice.toLowerCase()) {
+    console.log("It's a Draw!")
+  } else {
+    console.log("Else statement")
+
+  }
+}
+
 function computerPlay() {
     let choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random() * choice.length)]
 }
 
-console.log(computerPlay())
+
+playRound();
